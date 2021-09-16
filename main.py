78b827d5ebcee2c2e6a8f7826e565c7eb8c940fd
Sam Branka <sdbranka@me.com>
2021-09-16 16:26:38 -0400

    player faces left/right with travel

--- main.py
+++ main.py
@@ -10,12 +10,20 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Scrolling Shooter")
 
+# set frame rate
+CLOCK = pygame.time.Clock()
+FPS = 60
 
 
 # define player action variables
 moving_left = False
 moving_right = False
 
+# define colors
+BG = (144, 201, 120)
+
+def draw_bg():
+    screen.fill(BG)
 
 
 
@@ -23,7 +31,9 @@ class Soldier(pygame.sprite.Sprite):
     def __init__(self, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
         self.speed = speed
-        
+        # 1 (right) or -1 (left) determine if character is looking left or right
+        self.direction = 1
+        self.flip = False
         img = pygame.image.load("img/player/idle/0.png")
         self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
         self.rect = self.image.get_rect()
@@ -38,9 +48,15 @@ class Soldier(pygame.sprite.Sprite):
         # assign movement variable if moving left of right
         if moving_left:
             dx = -self.speed
+            self.flip = True
+            self.direction = -1
         if moving_right:
             dx = self.speed
-        
+            self.flip = False
+            self.direction = 1
+
+
+
         # update rectangle position
         self.rect.x += dx
         self.rect.y += dy
@@ -48,7 +64,14 @@ class Soldier(pygame.sprite.Sprite):
 
 
     def draw(self):
-        screen.blit(self.image, self.rect)
+
+        # determine direction to draw character facing and draw
+        img_to_flip = self.image
+        # when True player faces left
+        flip_in_x_axis = self.flip
+        flip_in_y_axis = False
+        pos_to_draw_at = self.rect
+        screen.blit(pygame.transform.flip(img_to_flip, flip_in_x_axis, flip_in_y_axis), pos_to_draw_at)
 
 
 
@@ -61,7 +84,13 @@ player = Soldier(200, 200, 3, 5)
 
 run = True
 while run:
+    CLOCK.tick(FPS)
+
+
     # display controls
+    # draw background color
+    draw_bg()
+
     # draw player
     player.draw()
     # move player
